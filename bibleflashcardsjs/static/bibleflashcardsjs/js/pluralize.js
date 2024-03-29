1dfc02a206030238978b2d229446af7b68df6fc4
alfloyd71 <alfloyd71@yahoo.com>
2024-03-28 23:12:23 -0400

    bug fix for boxes random verse

--- bibleflashcardsjs/static/bibleflashcardsjs/js/pluralize.js
+++ bibleflashcardsjs/static/bibleflashcardsjs/js/pluralize.js
@@ -1,24 +1,27 @@
-function pluralize(number, count) {
-    // Check if count is not 1, then pluralize the word
-    if (count !== 1) {
-        // Some basic pluralization rules
-        if (number.endsWith("y")) {
-            return number.slice(0, -1) + "ies"; // Replace 'y' with 'ies'
-        
-        } 
+console.log("This is a test from test-script")
 
-        if (number == 1) {
-          return number + "st";
-        } else if (number == 2){
-          return number + "nd";
-        } else if (number == 3) {
-          return number + "rd"
-        } else if (number == 4 || number == 5) {
-          return number + "th"
-        } else {
-          return number + "s";
-        }
+const pluralize=(word, number)=>{
+    console.log("hello from pluralize")
+    // Some basic pluralization rules
+    if (word.endsWith("y")) {
+        return word.slice(0, -1) + "ies"; // Replace 'y' with 'ies'
+    } 
+    
+    // Check for ordinal suffixes
+    switch (number) {
+        case 1:
+            return number + "st";
+        case 2:
+            return number + "nd";
+        case 3:
+            return number + "rd";
+        case 4:
+        case 5:
+            return number + "th";
+        default:
+            return number;
     }
-    // Return the word as it is if count is 1
-    return number;
-}
+  }
+
+    
+  
